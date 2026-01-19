2723b983a9b54bb0f5778771e23260d7b3b692df
Mustagiz Joad <Mustagiz@outvying.internal>
2026-01-20 00:24:18 +0530

    Update Firebase SDK to new project aleen-55164

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCD19s5igUtSoMyLPoQhia1vvs3IRzbu-0",
-    authDomain: "aleen-a02da.firebaseapp.com",
-    projectId: "aleen-a02da",
-    storageBucket: "aleen-a02da.firebasestorage.app",
-    messagingSenderId: "658986722156",
-    appId: "1:658986722156:web:2a8cdff7b7d7e3fee24949",
-    measurementId: "G-960HTTBH1C"
+    apiKey: "AIzaSyCSg3gx0Zs-MVlHypm7_3yj33fvxeQ4JLA",
+    authDomain: "aleen-55164.firebaseapp.com",
+    projectId: "aleen-55164",
+    storageBucket: "aleen-55164.firebasestorage.app",
+    messagingSenderId: "54437813504",
+    appId: "1:54437813504:web:2777b1c035aa1d263667e5",
+    measurementId: "G-KEQH31TQSM"
 };
 
 // Initialize Firebase
